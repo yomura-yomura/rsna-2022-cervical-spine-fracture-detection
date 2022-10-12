eb855b9ed21fa5120d8583bded6e03180839c984
jumpei.uchida <jumpei.uchida@brainpad.co.jp>
2022-10-12 12:28:36 +0000

    [new]

--- monai/save_3d_images.py
+++ monai/save_3d_images.py
@@ -1,9 +1,14 @@
 import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '/'))
 import CSFD.data.three_dimensions
 
 
 if len(sys.argv) == 1:
-    yaml_path = "resnet10.yaml"
+    yaml_path = "resnet10_make.yaml"
 else:
     yaml_path = sys.argv[1]
 
@@ -11,8 +16,8 @@ cfg_dataset = CSFD.data.load_yaml_config(yaml_path).dataset
 
 cfg_dataset.type_to_load = "npz"
 
-cfg_dataset.depth = 128
-cfg_dataset.image_2d_shape = [256, 256]
+cfg_dataset.depth = 256
+cfg_dataset.image_2d_shape = [512, 512]
 cfg_dataset.save_images_with_specific_depth = False
 
 cfg_dataset.height_range = [0.1, 0.8]
